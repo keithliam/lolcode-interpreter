6051e9ccfdc93fde119e8a6a8e5f71bec8d5cce9
Keith Liam Manaoto <keithliamm@gmail.com>
2017-11-29 12:47:11 +0800

    Added existing files

---
+++ js/init.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('select').material_select();
+    // // $('.button-collapse').sideNav();
+    // $('.parallax').parallax();
+    // // $('.modal').modal({
+    // //     endingTop: '15%'
+    // // });
+    $('#loginModal').modal({
+        endingTop: '20%'
+    });
+    //  $('#successModal').modal({
+    //     dismissible: false,
+    //     endingTop: '30%'
+    // });
+    // $('#messageModal').modal({
+    //     endingTop: '15%'
+    // });
+});
